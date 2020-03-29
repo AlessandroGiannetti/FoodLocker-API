a21ac637df39944fd4d95c0d003dac2621e298bb
AlessandroGiannetti <giannetti.1592006@studenti.uniroma1.it>
2020-03-29 18:53:25 +0200

    flatten array of posts response

--- app/controllers/followings_posts_controller.rb
+++ app/controllers/followings_posts_controller.rb
@@ -3,12 +3,12 @@ class FollowingsPostsController < ApplicationController
 
   # GET /users/:user_id/followings/posts
   def index
-    json_response(@posts)
+    json_response(@posts.flatten)
   end
 
   # GET /users/:user_id/posts/:id
   def show
-    json_response(@posts)
+    json_response(@posts.flatten)
   end
 
   private
@@ -19,7 +19,6 @@ class FollowingsPostsController < ApplicationController
     @posts = []
     p(@followings)
     @followings.each do |p|
-      puts(p['followed_id'])
       @posts << Post.where(user_id: p['followed_id']) if p['followed_id']
     end
   end
--- db/seeds.rb
+++
@@ -1,2 +0,0 @@
-User.create(name: "Alessandro", surname: "Giannetti", email: "ale.giannetti@hotmail.it", password: "123123", admin: "true")
-User.create(name: "Edoardo", surname: "Bini", email: "bini.edoardo@outlook.it", password: "123456")

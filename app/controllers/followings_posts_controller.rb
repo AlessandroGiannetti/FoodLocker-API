810579c758031b51b77100efce70713dd3a67afc
AlessandroGiannetti <giannetti.1592006@studenti.uniroma1.it>
2020-04-03 02:19:26 +0200

    likes functionality implemented - fix

--- app/controllers/followings_posts_controller.rb
+++ app/controllers/followings_posts_controller.rb
@@ -1,5 +1,6 @@
 class FollowingsPostsController < ApplicationController
   before_action :set_followers
+  before_action :set_post, only: [:update]
 
   # GET /users/:user_id/followings/posts
   def index
@@ -11,7 +12,16 @@ class FollowingsPostsController < ApplicationController
     json_response(@posts)
   end
 
+  def update
+    @post.write_attribute(:likes, @post.likes + 1)
+    @post.save
+    json_response(@post)
+  end
+
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def set_followers
 

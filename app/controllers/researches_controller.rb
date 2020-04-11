928478bab3b77032432c619201225556f326170c
AlessandroGiannetti <giannetti.1592006@studenti.uniroma1.it>
2020-04-11 15:33:27 +0200

    searching users

---
+++ app/controllers/researches_controller.rb
@@ -0,0 +1,19 @@
+class ResearchesController < ApplicationController
+  before_action :set_users
+
+  # GET /users
+  def index
+    json_response(@user)
+  end
+
+  private
+
+  def set_users
+    @user = User.select("relationships.follower_id, users.id, users.username, users.photo_profile")
+                .joins("LEFT JOIN relationships ON relationships.followed_id = users.id")
+                .where("relationships.follower_id = ? OR relationships.follower_id ~* ?", params[:user_id], '.*')
+                .uniq
+
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,10 @@ Rails.application.routes.draw do
   end
 
   resources :users do
+    resources :researches
+  end
+
+  resources :users do
     resources :weights
   end
 

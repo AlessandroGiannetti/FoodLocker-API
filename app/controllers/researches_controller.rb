ff076c87611f4f91b342f5fa48d7e0f2abce000c
AlessandroGiannetti <giannetti.1592006@studenti.uniroma1.it>
2020-04-11 15:57:29 +0200

    searching users on username fix

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -6,12 +6,16 @@ class ResearchesController < ApplicationController
     json_response(@user)
   end
 
+  def show
+    json_response(@user)
+  end
+
   private
 
   def set_users
     @user = User.select("relationships.follower_id, users.id, users.username, users.photo_profile")
                 .joins("LEFT JOIN relationships ON relationships.followed_id = users.id")
-                .where("relationships.follower_id = ? OR relationships.follower_id ~* ?", params[:user_id], '.*')
+                .where("(relationships.follower_id = ? OR relationships.follower_id ~* ?) AND users.username ~* ?", params[:user_id], '.*', params[:id])
                 .uniq
 
 
